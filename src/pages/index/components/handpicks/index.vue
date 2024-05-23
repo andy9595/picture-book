758bf4a02189208eed9ef28cd308bf323d29c9d2
梁耀安 <liangyaoan@zujibao.net>
2024-05-23 11:44:54 +0800

    style: 调整精选绘本样式

--- src/pages/index/components/handpicks/index.vue
+++ src/pages/index/components/handpicks/index.vue
@@ -39,11 +39,18 @@
 						<view class="book-img">
 							<image :src="bookItem.image" mode='aspectFill'></image>
 							<view class='book-type'>
-								<view class='language'>中文
+								<view class='language'>
+									<text v-if="bookItem.language==='cn'">中文</text>
+									<text v-else-if="bookItem.language==='en'">英文</text>
+								</view>
+								<view :class="['money-type',bookItem.read]">
+									<text v-if="bookItem.read==='free'">限免</text>
+									<text v-else-if="bookItem.read==='vip'">VIP</text>
 								</view>
 							</view>
 						</view>
 						<text class='diamond-title'>{{bookItem.title}}</text>
+						<view :class="['level',bookItem.language]">{{bookItem.difficulty_level}}</view>
 					</view>
 				</view>
 			</swiper-item>
@@ -77,12 +84,60 @@
 			.swiper-item {
 				display: flex;
 				flex-wrap: wrap;
+				padding-top: 4rpx;
 			}
 
 			.book-item {
 				width: 33%;
 				margin: 0 auto;
 				text-align: center;
+				position: relative;
+
+				.level {
+					position: absolute;
+					right: 10rpx;
+					top: 0rpx;
+					width: 30rpx;
+					height: 30rpx;
+					border-radius: 0 12rpx 0 12rpx;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					font-size: 16rpx;
+					font-weight: bold;
+					color: #fff;
+						box-shadow: -3rpx 6rpx 6rpx -1rpx rgba(0, 0, 0, 0.2);
+
+					&.en {
+						background-color: #308bfe;
+					}
+
+					&.cn {
+						background-color: #fd775b
+					}
+
+
+					/*right: 15rpx;
+					top: -1rpx;
+					font-size: 14rpx;
+					font-weight: bold;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					color: #fff;
+					--r: 8rpx;
+					border-inline: 5rpx solid #0000;
+					width: 18rpx;
+					padding: 2rpx 3rpx calc(var(--r) + 1rpx);
+					clip-path: polygon(0 0, 100% 0, 100% 100%, calc(100% - 5rpx) calc(100% - var(--r)), 50% 100%, 5rpx calc(100% - var(--r)), 0 100%);
+					background:
+						radial-gradient(50% .2em at top, #000a, #0000) border-box,
+						#bd1550 padding-box; */
+
+
+
+
+				}
 
 				/* 第二排 */
 				&:nth-child(n+4) {
@@ -92,18 +147,21 @@
 
 				.diamond-title {
 					font-size: 24rpx;
-					padding: 5rpx 16rpx;
+					padding: 0 16rpx;
 				}
 
 				.book-img {
+
 					position: relative;
 					width: 190rpx;
 					height: 230rpx;
 					margin: 0 auto;
-					margin-bottom: 4rpx;
+					margin-bottom: 16rpx;
+					border-radius: 12rpx;
+					overflow: hidden;
+					box-shadow: 3rpx 3rpx 6rpx 2rpx rgba(0, 0, 0, 0.2);
 
 					image {
-						border-radius: 10rpx;
 						width: 100%;
 						height: 100%;
 					}
@@ -118,8 +176,8 @@
 						width: 70%;
 
 						.language {
-							padding-left: 18rpx;
-							font-size: 22rpx;
+							padding-left: 16rpx;
+							font-size: 20rpx;
 							color: #fff;
 							display: flex;
 							align-items: center;
@@ -127,7 +185,37 @@
 							text-align: left;
 						}
 
+						.money-type {
+							position: absolute;
+							bottom: 0;
+							right: 0;
+							width: 70rpx;
+							height: 100%;
+							border-radius: 24rpx 0 0 0;
+							color: #fff;
+							font-size: 20rpx;
+							display: flex;
+							align-items: center;
+							justify-content: center;
+
+							&.free {
+								background-color: #ff8728;
+							}
+
+							&.vip {
+								background-color: #fed600;
+								color: #000;
+								font-weight: bold;
+
+								text {
+									padding-top: 2rpx;
+								}
+							}
+						}
+
 					}
+
+
 				}
 			}
 		}
